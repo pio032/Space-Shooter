9a1e9583588ccf95c33be5ccf227bff8ce605933
pio032 <pietro.ardi@gmail.com>
2024-12-12 22:14:26 +0100

    update file

--- Server_Socket.java
+++ Server_Socket.java
@@ -1,17 +1,24 @@
 import java.io.*;
-import java.net.*;  
-
+import java.net.*;
 
 public class Server_Socket {
-//verifica esistenza file utente
-    public static int FileUser(){
-    String path = "provaJava"; 
-    return 1;
+    // verifica esistenza file utente
+    public static int FileUser(String nomeUser) {
+
+        return 1;
     }
+
+    public static void CreationFile(String usr) throws IOException {
+        File a = new File(usr);
+        if (!a.exists()) {
+            FileWriter writer = new FileWriter(usr, true);
+        }
+    }
+
     public static void main(String[] args) {
         // Porta su cui il server ascolta
         int port = 1890;
-        boolean a=true;
+        boolean a = true;
 
         try {
             // Crea un ServerSocket che ascolta sulla porta specificata
@@ -22,38 +29,34 @@ public class Server_Socket {
             System.out.println("Client connesso da " + clientSocket.getInetAddress());
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            do{
-           
+            CreationFile(String.valueOf(clientSocket.getInetAddress()));
+            // inizio del ciclo
+            do {
+                // Crea un BufferedReader per leggere i dati inviati dal client
 
-            
-           
-            // Crea un BufferedReader per leggere i dati inviati dal client
-            
-            
-            // Legge un messaggio inviato dal client
-            String clientMessage = in.readLine();
-            System.out.println("Messaggio dal client: " + clientMessage);
+                // Legge un messaggio inviato dal client
+                String clientMessage = in.readLine();
+                System.out.println("Messaggio dal client: " + clientMessage);
 
-            // Invia una risposta al client e risposta 
-            
-            if(clientMessage.equals("stop")){
-            //stop server
-            a=false;
-            out.println("Ottimo! La nostra conversazione termina qui!");
-            }else{
-            out.println("Ciao, client! Ho ricevuto il tuo messaggio. Quello che ho ricevuto è: "+clientMessage);
-            }
-            
-            // Chiudi le connessioni e gli stream
-            
-            
-           
-        }while(a);
-        in.close();
-        out.close();
-        clientSocket.close();
-        serverSocket.close();
-        System.out.println("Connessione chiusa.");
+                // Invia una risposta al client e risposta
+
+                if (clientMessage.equals("stop")) {
+                    // stop server
+                    a = false;
+                    out.println("Ottimo! La nostra conversazione termina qui!");
+                } else {
+                    out.println(
+                            "Ciao, client! Ho ricevuto il tuo messaggio. Quello che ho ricevuto è: " + clientMessage);
+                }
+
+                // Chiudi le connessioni e gli stream
+
+            } while (a);
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+            System.out.println("Connessione chiusa.");
         } catch (IOException e) {
             e.printStackTrace();
         }
